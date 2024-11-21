ccfd43488e7944231d77f09393cd4bfd2961172f
Сережа <sokol16.90@mail.ru>
2024-11-21 21:39:46 +0400

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,30 @@
 plugins {
     id("java")
-    id("maven-publish") // Добавляем плагин для публикации
+    id("maven-publish") // Плагин для публикации
 }
 
-group = "com.github.SergeiSokolov123" // Убедитесь, что это правильный groupId
-version = "1.0.2"
+group = "com.github.SergeiSokolov123" // Ваш Group ID
+version = "1.0.2" // Версия библиотеки
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // Добавьте любые зависимости, которые нужны библиотеке
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+// Конфигурация публикации
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"]) // Экспорт java-компонентов
+        }
+    }
 }
